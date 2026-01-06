c72d93b978d7bd248b4b56ecc10bd87e8b38c485
HetPatel0 <hetp5852@gmail.com>
2026-01-06 10:02:12 +0530

    model changes

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <div className="bg-card w-fit rounded-2xl p-4 text-center text-2xl  font-semibold ">
-        about section not ready yet
+        About section not ready yet
       </div>
     </main>
   );
